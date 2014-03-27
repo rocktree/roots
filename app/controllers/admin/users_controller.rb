64196f87b3ebb1b433131530ef26da4050c0d442
Sean C. Davis <scdavis41@gmail.com>
2014-03-27 18:48:36 -0400

    update action for updating users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,16 @@
 class Admin::UsersController < AdminController
 
+  def update
+    p = update_params
+    p = no_pwd_params if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
+    if @item.update(p)
+      redirect_to @routes[:edit], :notice => "#{@model.to_s} was updated successfully."
+    else
+      @url = @routes[:show]
+      render :action => "edit"
+    end
+  end
+
   private
 
     def set_defaults
@@ -13,8 +24,12 @@ class Admin::UsersController < AdminController
         :is_admin)
     end
 
-    def edit_params
+    def update_params
       params.require(:user).permit(:password, :password_confirmation, :is_admin)
+    end
+
+    def no_pwd_params
+      params.require(:user).permit(:is_admin)
     end
 
 end
