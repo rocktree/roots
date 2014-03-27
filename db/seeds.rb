8b6e6e7c56c105ef4f93c4345f7cca73198dbdae
Sean C. Davis <scdavis41@gmail.com>
2014-03-27 07:26:49 -0400

    tasks for seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+Dir.glob("#{Rails.root}/app/models/*.rb").each { |file| require file }
+
+ActiveRecord::Base.descendants.each do |model|
+  filename = "#{Rails.root}/db/csv/#{model.to_s.tableize}.csv"
+  if File.exist?(filename)
+    CSV.foreach(filename, :headers => true) do |row|
+      attrs = {}
+      row.to_hash.each do |k,v|
+        if k =~ /.*\:file/
+          attrs[k.split(':')[0]] = File.open("#{Rails.root}/lib/assets/seeds/#{v}")
+        else
+          attrs[k] = v
+        end
+      end
+      model.create!(attrs.to_hash.symbolize_keys)
+    end
+    ActiveRecord::Base.connection.reset_pk_sequence!(model.to_s.tableize)
+  end
+end
