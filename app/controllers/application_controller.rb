0e910259e6eb648a709eb08da6c43b1ad742b6a1
Sean C. Davis <scdavis41@gmail.com>
2014-03-27 18:16:11 -0400

    devise routes in app controller private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery :with => :exception
 
-  def after_sign_in_path_for(resource)
-    if current_user.is_admin?
-      admin_dashboard_path
-    else
-      reports_path
-    end
-  end
+  private
 
-  def after_sign_out_path_for(user)
-    root_path
-  end
+    def after_sign_in_path_for(resource)
+      if current_user.is_admin?
+        admin_dashboard_path
+      else
+        reports_path
+      end
+    end
+
+    def after_sign_out_path_for(user)
+      root_path
+    end
 
 end
