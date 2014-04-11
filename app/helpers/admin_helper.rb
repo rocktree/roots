707f36bd5226c867f765edfe102788cd4a2019f7
Sean C Davis <scdavis41@gmail.com>
2014-04-11 14:29:56 -0400

    remove time helpers

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -89,33 +89,6 @@ module AdminHelper
     true
   end
 
-  # --------------------------------- Time
-
-  def months
-    [
-      ['January', 1],
-      ['February', 2],
-      ['March', 3],
-      ['April', 4],
-      ['May', 5],
-      ['June', 6],
-      ['July', 7],
-      ['August', 8],
-      ['September', 9],
-      ['October', 10],
-      ['November', 11],
-      ['December', 12],
-    ]
-  end
-
-  def years
-    years = []
-    5.times do |idx|
-      years << (Date.today.year - idx).to_i
-    end
-    years
-  end
-
   # --------------------------------- Misc
 
   def icon_to(icon, path, options = {})
